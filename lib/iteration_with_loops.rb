74d9a6d2ae9baae8400564207fb6a98697fbf517
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-07 17:31:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,8 @@ def join_nested_strings(src)
     string = ""
     element_index = 0
     while element_index < src[row_index].count do
-      element_index += 1
       new_array.push(string)
+      element_index += 1
     end
     row_index += 1
   end
